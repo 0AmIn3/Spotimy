153be8d8307b11e34faf0c2177a731d19da08e4b
Daler <dsarifkulo021@gmail.com>
2023-03-27 18:46:10 +0500

    created

---
+++ src/pages/Doc.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Doc = () => {
+    return (
+        <div>
+            <h1>
+                DOC 
+            </h1>
+        </div>
+    );
+};
+
+export default Doc;
