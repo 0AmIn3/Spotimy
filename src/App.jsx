82df2876a7cacf898596d19842a97bed06c5a685
Badriddin <frozeshkh@gmail.com>
2023-03-28 11:42:38 +0500

    created new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import React from "react";
+import ItemBlog from "./.childs/ItemBlog";
+
 
 function App() {
-	return (
-		<div></div>
-	)
+  return (
+    <div className="w-[100%]">
+      <div className="gap-[20px] grid  grid-cols-4 p-[20px]  bg-[#121212] ">
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+        <ItemBlog />
+      </div>
+    </div>
+  );
 }
 
 export default App;
