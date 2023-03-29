83b490986d42382ff7a483e1dd8d9f705fcc207d
kennard-ll <abrorkulovfariz777@gmail.com>
2023-03-29 16:42:34 +0500

    do smth

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -7,22 +7,24 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Songs from './Songs';
+import "./../style.scss"
 
 
 export default function List() {
   return (
-    <div className='max-w-[80%] flex place-content-end'>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell># TITLE</TableCell>
+    <div className='max-w-[80%]'>
+    <TableContainer className='px-8' component={Paper}>
+      <Table >
+        <TableHead className='px-2'>
+          <TableRow className='head-row'>
+            <TableCell>#</TableCell>
+            <TableCell>TITLE</TableCell>
             <TableCell align="right">ALBUM</TableCell>
             <TableCell align="right">DATE ADDED</TableCell>
             <TableCell align="right"><img src="/images/clock.svg" alt="" /></TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
+        <TableBody className='px-2'>
             <Songs />
             <Songs />
             <Songs />
--- src/component/Songs.jsx
+++ src/component/Songs.jsx
@@ -2,16 +2,25 @@ import * as React from 'react';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import { Audio } from  'react-loader-spinner'
+import "./../style.scss"
 
 const Songs = () => {
     return ( 
         <>
-            <TableRow  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row"></TableCell>
-              <TableCell align="right">hfhfh</TableCell>
-              <TableCell align="right">fgf</TableCell>
-              <TableCell align="right">ffghfgh</TableCell>
-              <TableCell align="right">fghfhfghgfhgfhgfgh</TableCell>
+            <TableRow  className='body-row'>
+              <TableCell component="th" scope="row"><Audio   height="28" width="28" /></TableCell>
+              <TableCell align="right">
+                <div className='flex'>
+                    <img src="/images/Screenshot 2022-06-04 at 20.05.svg" alt="" />
+                    <div>
+                      <h6>Play It Safe</h6>
+                      <p>Julia Wolf</p>
+                    </div>
+                </div>
+              </TableCell>
+              <TableCell align="right">Play It Safe</TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right">2 : 12</TableCell>
             </TableRow>
 
         </>
