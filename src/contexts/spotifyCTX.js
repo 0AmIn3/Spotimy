f73193214e1ac95254e1cf5c95bd4e395d5944af
alitolibov <alitolibov066@gmail.com>
2023-04-06 17:08:43 +0500

    fixxxed

--- src/contexts/spotifyCTX.js
+++ src/contexts/spotifyCTX.js
@@ -5,7 +5,7 @@ import {
 
 export const spotify = createContext({
     client_id: "919136d06615483ba8d995792fec91e3",
-    REDIRECT_URI: "http://localhost:5173/",
+    REDIRECT_URI: "http://localhost:5176/",
     AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
     RESPONSE_TYPE: "token"
 })
