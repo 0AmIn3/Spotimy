884936a77213de462a2d50ebc842c8f2d07d209e
0Amin3 <aminfedya2888@gmail.com>
2023-04-12 15:37:44 +0500

    fff

--- src/contexts/spotifyCTX.js
+++ src/contexts/spotifyCTX.js
@@ -3,11 +3,11 @@ import {
 } from "react";
 
 const actualURL = import.meta.url.split('/src')[0]
-
+console.log(import.meta.env.VITE_API_KEY);
 console.log(actualURL);
 
 export const spotify = createContext({
-    client_id: import.meta.env.VITE_API_KEY,
+    client_id: '914d069760c541d9b9f93c5ca4e66b50',
     REDIRECT_URI: actualURL,
     AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
     RESPONSE_TYPE: "token"
